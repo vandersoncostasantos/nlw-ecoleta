1fe57955be29f0ec53fde71cbc36e650d1575c88
Vanderson Santos <vsantos@compleo.com.br>
2020-06-26 12:05:33 -0300

    Day4 - Doing a little bit more

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'react-native';
 import { AppLoading } from 'expo';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+import {
+  ThemeProvider,
+  DefaultTheme,
+  Types,
+} from './src/styles/styled-components';
 
 import Routes from './src/routes';
 
+import { LightTheme } from './src/styles/themes/Themes';
+
 export default function App() {
+  const [theme, setTheme] = useState<Types>(LightTheme);
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_500Medium,
@@ -18,13 +26,13 @@ export default function App() {
   }
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar
         barStyle="dark-content"
         backgroundColor="transparent"
         translucent
       ></StatusBar>
       <Routes></Routes>
-    </>
+    </ThemeProvider>
   );
 }
--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Feather as Icon } from '@expo/vector-icons';
-import { View, ImageBackground, Text, Image } from 'react-native';
+import { View, StyleSheet, ImageBackground, Text, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 
-import {
-  Container,
-  Main,
-  Title,
-  Description,
-  Footer,
-  Button,
-  ButtonIcon,
-  ButtonText,
-  Input,
-} from './styles';
+// import {
+//   Container,
+//   Main,
+//   Title,
+//   Description,
+//   Footer,
+//   Button,
+//   ButtonIcon,
+//   ButtonText,
+//   Input,
+// } from './styles';
 
 const Home = () => {
   const navigation = useNavigation();
@@ -25,26 +25,26 @@ const Home = () => {
 
   return (
     <ImageBackground
-      source={require('.../.../assets/home-background.png')}
-      style={Container}
+      source={require('../../assets/home-background.png')}
+      style={styles.container}
       imageStyle={{ width: 274, height: 368 }}
     >
-      <View style={Main}>
+      <View style={styles.main}>
         <Image source={require('../../assets/logo.png')}></Image>
-        <Text style={Title}>Seu marketplace de coleta de resíduos</Text>
-        <Text style={Description}>
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>
           Ajudamos pessoas a encontrar pontos de coleta de forma eficiente.
         </Text>
       </View>
 
-      <View style={Footer}>
-        <RectButton style={Button} onPress={handleNavigationToPoints}>
-          <View style={ButtonIcon}>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleNavigationToPoints}>
+          <View style={styles.buttonIcon}>
             <Text>
               <Icon name="arrow-right" color="#FFF" size={24}></Icon>
             </Text>
           </View>
-          <Text style={ButtonText}>Entrar</Text>
+          <Text style={styles.buttonText}>Entrar</Text>
         </RectButton>
       </View>
     </ImageBackground>
@@ -52,3 +52,72 @@ const Home = () => {
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#322153',
+    fontSize: 32,
+    fontFamily: 'Ubuntu_700Bold',
+    maxWidth: 260,
+    marginTop: 64,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 16,
+    fontFamily: 'Roboto_400Regular',
+    maxWidth: 260,
+    lineHeight: 24,
+  },
+
+  footer: {},
+
+  select: {},
+
+  input: {
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
+  button: {
+    backgroundColor: '#34CB79',
+    height: 60,
+    flexDirection: 'row',
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+
+  buttonIcon: {
+    height: 60,
+    width: 60,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#FFF',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  },
+});
